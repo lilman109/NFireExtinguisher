f3b8e8d03171eda57a6c165145fccdd9022cf6f4
lilman109 <akira.yoshidasjsu@gmail.com>
2021-05-23 23:31:12 +0900

    added header and large card component

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <Hero />
+    <LargeCardDisplay
+      v-for="cardInfo in largeCardInfo"
+      :key="cardInfo.id"
+      :cardsSection="cardInfo"
+    />
+  </div>
+</template>
+
+<script>
+import { largeCardSections } from "@/assets/data.js";
+export default {
+  data() {
+    return {
+      largeCardInfo: largeCardSections
+    };
+  }
+};
+</script>
+
+<style></style>
